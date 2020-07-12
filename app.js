bf75177c685c2b6b5b59f3c582e789c7ccac0850
Takuro Kayamoto <takurokayamoto@MacBook-Pro.local>
2020-07-12 19:09:51 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//jshint esversion:6
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const _ = require("lodash");
+const { Mongoose } = require("mongoose");
+const app = express();
+
+app.set("view engin", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+Mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true, useUnifiedTopology: true })
+
+app.get("/", function (req, res) { 
+    res.render("home");
+ });
+
+
+ app.listen(3000, function () { 
+     console.log("the server is now running on 3000");
+  })
